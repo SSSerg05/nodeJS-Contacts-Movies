3a5806b07fa601c0eb0a1292a542c879cfd88bc1
SSSerg05 <ssserg05@gmail.com>
2023-11-09 16:39:35 +0200

    fix

--- routes/api/movies-route.js
+++ routes/api/movies-route.js
@@ -3,6 +3,8 @@ import express from 'express';
 import ctrl from "../../controllers/ctrl-contacts.js";
 const moviesRouter = express.Router();
 
+moviesRouter.get("/" ), 
+
 
 
 export default moviesRouter;
